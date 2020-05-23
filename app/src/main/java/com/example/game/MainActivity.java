c3a5227d90f679847d0403d921858338fb9c9d42
AlexZhangUPUPUP <64730887+AlexZhangUPUPUP@users.noreply.github.com>
2020-05-23 23:34:04 +1000

    The basci version of the game
    The basci version of the game

---
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button strat;
+    Button history;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        strat =findViewById(R.id.start);
+        history=findViewById(R.id.history);
+
+
+        strat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =new Intent( MainActivity.this, first_game.class);
+                startActivity(intent);
+            }
+        }); //jump to the game
+
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });//jump to history
+    }
+}
