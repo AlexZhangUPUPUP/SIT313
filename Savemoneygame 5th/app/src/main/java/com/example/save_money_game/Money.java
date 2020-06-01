94a2c9893b203775c2bef5da897c6f3308bcda90
AlexZhangUPUPUP <64730887+AlexZhangUPUPUP@users.noreply.github.com>
2020-06-01 23:57:30 +1000

    The 5th version
    Fix some bugs

---
+++ Savemoneygame 5th/app/src/main/java/com/example/save_money_game/Money.java
@@ -0,0 +1,19 @@
+package com.example.save_money_game;
+
+public class Money {
+
+    int number;
+
+    public Money( int i){
+
+        number =i;
+    }
+
+    public int Returnnumber(){
+
+        return number;
+
+    }
+}
+
+
