a5b844952aed44d97169aa305d55487ba33da193
AlexZhangUPUPUP <64730887+AlexZhangUPUPUP@users.noreply.github.com>
2020-06-04 23:58:55 +1000

    7th verion of the game
    7th version fo the game

---
+++ Savemoneygame 7th/app/src/main/java/com/example/save_money_game/Money.java
@@ -0,0 +1,50 @@
+package com.example.save_money_game;
+
+public class Money {
+
+    int leftMargin;
+    int TopMargin;
+    int core;
+
+
+    public Money( int i){
+
+        leftMargin =i;
+    }
+
+    public Money( int left, int Top){
+
+        leftMargin =left;
+        TopMargin  = Top;
+    }
+
+    public Money( int left, int Top, int Core){
+
+        leftMargin =left;
+        TopMargin  = Top;
+        core       = Core;
+    }
+
+    public int ReturnLeftMargin(){
+
+
+        return leftMargin;
+
+    }
+
+    public int ReturnCore(){
+
+
+        return core;
+
+    }
+
+    public int ReturnTop(){
+
+
+        return TopMargin;
+
+    }
+}
+
+
